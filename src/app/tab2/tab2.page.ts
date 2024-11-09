471c86fefaa3770ee6848493449e9909db595256
Daniel Sogl <me@danielsogl.de>
2024-11-09 08:13:34 +0100

    feat: add local notification demo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class Tab2Page {
+  private notificationId = 1;
 
-  constructor() {}
+  protected async scheduleNotification(): Promise<void> {
+    await LocalNotifications.requestPermissions();
 
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          title: 'Test notification',
+          body: 'Test notification',
+          id: this.notificationId,
+          schedule: { at: new Date(Date.now() + 1000 * 5) },
+        },
+      ],
+    });
+
+    this.notificationId++;
+  }
 }
